4aed25b8f939b471eee1b0e982b64757d822a50b
alban veaute <alban.veaute@gmail.com>
2023-11-21 11:58:40 +0100

    add validates on dream model

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,4 +1,10 @@
 class Dream < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :price, presence: true
+  validates :availability, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
+  validates :category, presence: true
 end
