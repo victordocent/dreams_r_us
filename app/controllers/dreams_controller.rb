74fd1729bb92e4fbbc74dc50be7849d4e722f54b
Nicolas Timillero <nicolas.timillero@gmail.com>
2023-11-21 17:00:50 +0100

    added links on navbar and put show available for visitors

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,5 +1,5 @@
 class DreamsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :find_dream, only: [:show, :edit, :update, :destroy]
 
   def index
