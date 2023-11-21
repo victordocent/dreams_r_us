286075b8152afaa6fdb094e4fa56b0c67d2b4de0
alban veaute <alban.veaute@gmail.com>
2023-11-21 17:25:43 +0100

    add seed workinggaagaa

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem "devise"
 gem "dotenv-rails"
 gem "cloudinary"
 
+gem "faker"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+require "open-uri"
+
+Dream.destroy_all
+User.destroy_all
+
+user1 = User.create!(
+  email: "wagon@gmail.com",
+  password: "LeWaWa01"
+)
+
+user2 = User.create!(
+  email: "wawa@gmail.com",
+  password: "LeWaWa02"
+)
+
+users = [user1, user2]
+
+10.times do
+
+  dream = Dream.new(
+    title: Faker::JapaneseMedia::CowboyBebop.episode,
+    price: rand(1..1_000_000),
+    availability: true,
+    description: Faker::JapaneseMedia::CowboyBebop.quote,
+    category: Faker::University.name,
+    user: users.sample
+  )
+
+  file = URI.open(Faker::LoremFlickr.image(search_terms: [Faker::JapaneseMedia::CowboyBebop.episode].map { |item| item.gsub(' ', '_') }))
+  dream.photos.attach(io: file, filename: "#{Faker::JapaneseMedia::CowboyBebop.episode}.png", content_type: "image/png")
+  dream.save
+end
