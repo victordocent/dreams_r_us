aedc2f6c353668465c4e52f3a4ead989eaec935f
alban veaute <alban.veaute@gmail.com>
2023-11-24 15:42:21 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,106 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-require 'faker'
+require "faker"
 require "open-uri"
-
 Dream.destroy_all
 User.destroy_all
-
-user1 = User.create!(
-  email: "wagon@gmail.com",
-  password: "LeWaWa01"
+nicolas = User.create!(
+  email: "nicolas@gmail.com",
+  password: "nicolas"
 )
-
-user2 = User.create!(
-  email: "wawa@gmail.com",
-  password: "LeWaWa02"
+anahita = User.create!(
+  email: "anahita@gmail.com",
+  password: "anahita"
 )
-
-users = [user1, user2]
-
-10.times do
-
-  dream = Dream.new(
-    title: Faker::JapaneseMedia::CowboyBebop.episode,
-    price: rand(1..1_000_000),
-    availability: true,
-    description: Faker::JapaneseMedia::CowboyBebop.quote,
-    category: Faker::University.name,
-    user: users.sample
+dream_bf = Dream.new(
+  title: "Black Friday",
+  price: 1000,
+  availability: true,
+  description: Faker::JapaneseMedia::CowboyBebop.quote,
+  category: "Nightmares",
+  user: anahita
   )
+picture_urls = ["https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/cgmebfxmji5rjevkised.jpg",
+                "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/xxejtshwmrsmxolltxof.webp",
+                "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/z5gdk1jev6cs7ggfadfz.jpg",
+                "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833089/dreams_r_us/djsoyvw33oqvdltldk4u.jpg",
+                "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833089/dreams_r_us/yidcmu4xewymlwuqzafa.jpg"]
+picture_urls.each do |url|
+  file = URI.open(url)
+  dream_bf.photos.attach(io: file, filename: "#{Faker::JapaneseMedia::CowboyBebop.episode}.png", content_type: 'image/jpg')
+  dream_bf.save
+end
 
-  5.times do
-    file = URI.open(Faker::LoremFlickr.image(search_terms: [Faker::JapaneseMedia::CowboyBebop.episode].map { |item| item.gsub(' ', '_') }))
-    dream.photos.attach(io: file, filename: "#{Faker::JapaneseMedia::CowboyBebop.episode}.png", content_type: "image/png")
-    dream.save
-  end
+dream_ast = Dream.new(
+  title: "Australie",
+  price: 250,
+  availability: true,
+  description: Faker::JapaneseMedia::CowboyBebop.quote,
+  category: "Travel",
+  user: anahita
+  )
+picture_urls = ["https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833092/dreams_r_us/jzb6dtls4xqfbvljsxdz.jpg",
+                "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833091/dreams_r_us/vu4hixysej8db6lvllyi.jpg",
+                "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/z0nmyxfqwvraudivw4ig.jpg",
+                "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/g3o3tysqtdqswy5fy3sj.jpg",
+                "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700832809/dreams_r_us/gec7g8khp5v4flmjdkpj8no2mq1k.jpg"]
+picture_urls.each do |url|
+  file = URI.open(url)
+  dream_ast.photos.attach(io: file, filename: "#{Faker::JapaneseMedia::CowboyBebop.episode}.png", content_type: 'image/jpg')
+  dream_ast.save
+end
+
+dream_mtn = Dream.new(
+  title: "Moutain escape",
+  price: 650,
+  availability: true,
+  description: Faker::JapaneseMedia::CowboyBebop.quote,
+  category: "Astral",
+  user: anahita
+  )
+  picture_urls = ["https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833088/dreams_r_us/b4gn0deaneczqo0eiziv.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833089/dreams_r_us/o9vbez8nx1bvrfhk8oxa.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833091/dreams_r_us/hnq6adzrfgzn9pgbs1bw.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833087/dreams_r_us/imb67an08ldijwi2jh1r.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/kihxzic1h0iourmcxqto.webp"]
+picture_urls.each do |url|
+  file = URI.open(url)
+  dream_mtn.photos.attach(io: file, filename: "#{Faker::JapaneseMedia::CowboyBebop.episode}.png", content_type: 'image/jpg')
+  dream_mtn.save
+end
+
+dream_teath = Dream.new(
+  title: "J'ai les dents qui tombent!!!!!!!!!!",
+  price: 2000,
+  availability: true,
+  description: Faker::JapaneseMedia::CowboyBebop.quote,
+  category: "Falling",
+  user: anahita
+  )
+  picture_urls = ["https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/pumrhsjcp2s2vz209kkd.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833089/dreams_r_us/o9vbez8nx1bvrfhk8oxa.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833091/dreams_r_us/hnq6adzrfgzn9pgbs1bw.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833087/dreams_r_us/imb67an08ldijwi2jh1r.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/kihxzic1h0iourmcxqto.webp"]
+picture_urls.each do |url|
+  file = URI.open(url)
+  dream_teath.photos.attach(io: file, filename: "#{Faker::JapaneseMedia::CowboyBebop.episode}.png", content_type: 'image/jpg')
+  dream_teath.save
+end
+
+dream_middle_earth = Dream.new(
+  title: "Aragorn dead LoL xD",
+  price: 100,
+  availability: true,
+  description: Faker::JapaneseMedia::CowboyBebop.quote,
+  category: "Initiative",
+  user: anahita
+  )
+  picture_urls = ["https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833088/dreams_r_us/hx5lyv5ehfycf92lvagn.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833089/dreams_r_us/o9vbez8nx1bvrfhk8oxa.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833091/dreams_r_us/hnq6adzrfgzn9pgbs1bw.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833087/dreams_r_us/imb67an08ldijwi2jh1r.jpg",
+                  "https://res.cloudinary.com/dcvyn1qtm/image/upload/v1700833086/dreams_r_us/kihxzic1h0iourmcxqto.webp"]
+picture_urls.each do |url|
+  file = URI.open(url)
+  dream_middle_earth.photos.attach(io: file, filename: "#{Faker::JapaneseMedia::CowboyBebop.episode}.png", content_type: 'image/jpg')
+  dream_middle_earth.save
 end
